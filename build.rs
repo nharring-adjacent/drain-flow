a18d78eb747ee9d98380c7feb4a7d341734d1f6b
Nick Harring <furiouseyebrows@gmail.com>
2025-06-12 16:50:23 -0700

    fix build

--- build.rs
+++ build.rs
@@ -2,8 +2,7 @@ use parol::build::Builder;
 
 fn main() {
     // Generate parser and user trait code using parol
-    Builder::with_cargo_script_output().with_grammar_file("src/query/logql.par")
-        .unwrap() // Handle error appropriately in real code
-        .generate_parser_source()
+    Builder::with_cargo_script_output().grammar_file("src/query/logql.par")
+        .generate_parser()
         .unwrap(); // Handle error
 }
